247fed7ea5dbe3dc340e9477fdc4211c4d6143ec
Gabriel V. Vian <gabriel.vian@unesp.br>
2025-06-24 00:09:06 -0300

    Update ttHHanalyzer_trigger.cc

--- ttHHanalyzer_trigger.cc
+++ ttHHanalyzer_trigger.cc
@@ -815,6 +815,7 @@ void ttHHanalyzer::analyze(event *thisEvent) {
 
 
 void ttHHanalyzer::process(event* thisEvent, sysName sysType, bool up){
+     _weight = 1.0;
     createObjects(thisEvent, sysType, up);
     if(!selectObjects(thisEvent))  return;
 // Inicializar SF de trigger
