462039a4032a28242c73ac15d2bf4c950fe831b1
Gabriel V. Vian <gabriel.vian@unesp.br>
2025-08-10 20:28:05 -0300

    Update ttHHanalyzer_trigger.h

--- ttHHanalyzer_trigger.h
+++ ttHHanalyzer_trigger.h
@@ -21,8 +21,10 @@
 #include "include/tthHypothesisCombinatorics.h"
 #include "include/HypothesisCombinatorics.h"
 #include "fifo_map.hpp"
-#include "json.hpp" // muon trigger SF (TSFmu)
-using json = nlohmann::json;  /// this is for MUON trigger SF (TSFmu)
+
+extern json muonTrigSFJson;
+//#include "json.hpp" // muon trigger SF (TSFmu)
+//using json = nlohmann::json;  /// this is for MUON trigger SF (TSFmu)
 
 //using namespace ROOT::Math;
 using nlohmann::fifo_map;
