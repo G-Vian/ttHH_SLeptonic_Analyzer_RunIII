1dfff028d3cc2d9c91fee69058ab4708e03e736b
Gabriel V. Vian <gabriel.vian@unesp.br>
2025-08-11 14:22:08 -0300

    Update Ploter3.cpp

--- Ploter3.cpp
+++ Ploter3.cpp
@@ -433,6 +433,7 @@ int Ploter3() {
 //{"TTZ", {"TTZ.root", false, kAzure+1, "TTZ"}},
 {"TTH", {"TTH.root", false, kPink+1, "TTH"}},
 {"TT4b", {"TT4b.root", false, kSpring+3, "TT4b"}},
+{"TTbb", {"TTbb.root", false, kViolet+1, "TTbb"}},
 {"TTZH", {"TTZH.root", false, kTeal+1, "TTZH"}},
 {"TTZZ", {"TTZZ.root", false, kRed+1, "TTZZ"}},
 {"TTHH", {"TTHH.root", true, kBlack, "TTHH"}},
