14e88ab933b6f9a366e5660bc66ad15139316b70
Gabriel V. Vian <gabriel.vian@unesp.br>
2025-09-13 07:56:01 -0300

    Update ElectronEnergyCalibrator.h

--- src/ElectronEnergyCalibrator.h
+++ src/ElectronEnergyCalibrator.h
@@ -6,12 +6,13 @@
 #include <random>
 #include <memory>
 #include <correction.h>
-//#include "ttHHanalyzer_trigger.h"  // para LOG_INTERVAL, _DataOrMC, _year etc.
 
 class ElectronEnergyCalibrator {
 public:
+    // Constructor: recebe caminho para JSON, tipo de dado (Data ou MC) e ano
     ElectronEnergyCalibrator(const std::string& jsonPath, const std::string& dataOrMC, int year);
 
+    // Aplica calibração nos elétrons
     void applyElectronCalibration(
         std::vector<float>& Electron_pt,
         const std::vector<float>& Electron_eta,
@@ -19,14 +20,14 @@ public:
         const std::vector<int>& Electron_seedGain,
         unsigned int runNumber,
         long long eventNumber,
-        int isMC // flag para diferenciar MC (smearing) de DATA (scale)
+        int isMC
     );
 
 private:
     std::unique_ptr<correction::CorrectionSet> cset;
     std::string _dataOrMC;
     int _year;
-    mutable std::mt19937 rng;
+    mutable std::mt19937 rng; // gerador aleatório para smearing MC
 };
 
 #endif
