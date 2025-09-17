80c7692a99656df77b2bb2d986bf3dfc8ffcedb9
Gabriel V. Vian <gabriel.vian@unesp.br>
2025-09-17 11:38:06 -0300

    Update ElectronEnergyCalibrator.h

--- src/ElectronEnergyCalibrator.h
+++ src/ElectronEnergyCalibrator.h
@@ -1,18 +1,16 @@
-#pragma once
+#ifndef ELECTRON_ENERGY_CALIBRATOR_H
+#define ELECTRON_ENERGY_CALIBRATOR_H
 
 #include <vector>
 #include <string>
 #include <random>
 #include <memory>
 #include <correction.h>
-#include <stdexcept>
 
 class ElectronEnergyCalibrator {
 public:
-    // Construtor: dataOrMC = "DATA" ou "MC", year = "2022", "2023", "2024", etc.
     ElectronEnergyCalibrator(const std::string& year, const std::string& dataOrMC);
 
-    // Aplica calibração nos vetores de elétrons
     void applyElectronCalibration(
         std::vector<float>& Electron_pt,
         const std::vector<float>& Electron_eta,
@@ -20,17 +18,17 @@ public:
         const std::vector<int>& Electron_seedGain,
         unsigned int runNumber,
         long long eventNumber,
-        int isMC // 1 para MC (smearing), 0 para DATA (scale)
+        bool isMC
     );
 
 private:
-    std::unique_ptr<correction::CorrectionSet> cset;
+    std::string _year;
     std::string _dataOrMC;
-    std::string _year;          // agora é string
+    std::unique_ptr<correction::CorrectionSet> cset;
     mutable std::mt19937 rng;
 
-    // Retorna o caminho absoluto do JSON dependendo do ano e data/MC
     std::string getElectronJSONPath() const;
+    std::string getCompoundName() const;
 };
 
-
+#endif
