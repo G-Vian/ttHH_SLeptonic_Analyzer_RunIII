5b073adb5d9d6839f311f0134220d58116a2cd75
Gabriel V. Vian <gabriel.vian@unesp.br>
2025-09-18 15:41:00 -0300

    Update ElectronEnergyCalibrator.h

--- src/ElectronEnergyCalibrator.h
+++ src/ElectronEnergyCalibrator.h
@@ -1,17 +1,19 @@
 #pragma once
 
-#include <string>
 #include <vector>
+#include <string>
 #include <memory>
-#include <variant>
 #include <random>
-#include <correction.h>
+#include <variant>
+#include <iostream>
+
+#include "correction.h"
 
 class ElectronEnergyCalibrator {
 public:
     ElectronEnergyCalibrator(const std::string& year, const std::string& dataOrMC);
 
-    // Aplica a calibração nos vetores de elétrons
+    // Função principal para aplicar a calibração
     void calibrateElectrons(
         std::vector<float>& pts,
         const std::vector<float>& etas,
@@ -20,14 +22,12 @@ public:
         int runNumber
     );
 
-    // Recupera os limites válidos do JSON
-    float getMin(const std::string& variable) const;
-    float getMax(const std::string& variable) const;
+    float getMin(const std::string& var) const;
+    float getMax(const std::string& var) const;
 
 private:
     std::string _year;
     std::string _dataOrMC;
-
     std::unique_ptr<correction::CorrectionSet> cset;
     std::mt19937 rng;
 
