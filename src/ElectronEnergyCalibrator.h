bbf4213d14dfbf6164b0768b39ec723cbc7b567c
Gabriel V. Vian <gabriel.vian@unesp.br>
2025-09-18 21:18:30 -0300

    Update ElectronEnergyCalibrator.h

--- src/ElectronEnergyCalibrator.h
+++ src/ElectronEnergyCalibrator.h
@@ -6,12 +6,13 @@
 #include <random>
 #include <variant>
 #include <iostream>
+
 #include "correction.h"
 
 class ElectronEnergyCalibrator {
 public:
-    // Construtor: apenas inicializa CorrectionSet e RNG
-    ElectronEnergyCalibrator();
+    // Construtor com parâmetros
+    ElectronEnergyCalibrator(const std::string& year, const std::string& dataOrMC);
 
     // Função principal para aplicar a calibração
     void calibrateElectrons(
@@ -19,18 +20,17 @@ public:
         const std::vector<float>& etas,
         const std::vector<float>& r9s,
         const std::vector<int>& gains,
-        int runNumber,
-        const std::string& year,
-        const std::string& DataOrMC
+        int runNumber
     );
 
     float getMin(const std::string& var) const;
     float getMax(const std::string& var) const;
 
 private:
+    std::string _year;
+    std::string _DataOrMC;
     std::unique_ptr<correction::CorrectionSet> cset;
     std::mt19937 rng;
 
-    std::string getElectronJSONPath(const std::string& year, const std::string& DataOrMC) const;
+    std::string getElectronJSONPath() const;
 };
-
